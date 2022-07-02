8c0131e6404c04da71d1bac6ca58f4667cbcdcb7
revamugi <108567702+revamugi@users.noreply.github.com>
2022-07-02 13:19:05 +0530

    Add files via upload

---
+++ mugesh.py
@@ -0,0 +1,32 @@
+import sqlite3
+con=sqlite3.connect('user.db')
+#con.execute("create table users(NAME,ACTOR,ACTRESS,DIRECTOR,YEAR);")
+def insertData(name,actor,actress,director,year):
+    qry=f'insert into users (NAME,ACTOR,ACTRESS,DIRECTOR,YEAR) values ("{name}","{actor}","{actress}","{director}","{year}");'
+    con.execute(qry)
+    con.commit()
+    print("add")
+
+def select(actor):
+    qry='select * from users where actor="'+actor+'";'
+    result=con.execute(qry)
+    for row in result:
+        print(row)
+
+ch="y"
+while(ch.lower()!='n'):
+    c=int(input())
+    if(c==1):
+        print("add record")
+        name=input("Enter the name of the movie:")
+        actor=input("Enter the name of the actor:")
+        actress=input("Enter the name of the actress:")
+        director=input("Enter the name of the director:")
+        year=input("Enter  the year:")
+        insertData(name,actor,actress,director,year)
+    elif (c==2):
+        print("select")
+        select(input("ACTOR NAME:"))
+    else:
+        print("Invalied option:" + c)
+    ch = input("Do you want to continue[Y/n]: ")
